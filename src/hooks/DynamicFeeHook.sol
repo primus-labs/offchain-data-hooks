9d38a5b9f1af8c5a3deb4310ae9f73e12b7f3327
fksyuan <fksyuan@163.com>
2023-10-13 13:58:02 +0800

    fork

---
+++ src/hooks/DynamicFeeHook.sol
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import {IDynamicFeeManager} from "@uniswap/v4-core/contracts/interfaces/IDynamicFeeManager.sol";
+import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
+import {Hooks} from "@uniswap/v4-core/contracts/libraries/Hooks.sol";
+import {PoolKey, PoolIdLibrary} from "@uniswap/v4-core/contracts/types/PoolId.sol";
+import {BalanceDelta} from "@uniswap/v4-core/contracts/types/BalanceDelta.sol";
+import {BaseHook} from "v4-periphery/BaseHook.sol";
+
+import {BaseFactory} from "../BaseFactory.sol";
+
+contract DynamicFeeHook is BaseHook, IDynamicFeeManager {
+    using PoolIdLibrary for PoolKey;
+
+    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
+
+    function getHooksCalls() public pure override returns (Hooks.Calls memory) {
+        return Hooks.Calls({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeModifyPosition: true,
+            afterModifyPosition: true,
+            beforeSwap: true,
+            afterSwap: true,
+            beforeDonate: false,
+            afterDonate: false
+        });
+    }
+
+    /// @notice The dynamic fee manager determines fees for pools
+    /// @dev note that this pool is only called if the PoolKey fee value is equal to the DYNAMIC_FEE magic value
+    function getFee(address sender, PoolKey calldata key, IPoolManager.SwapParams calldata params, bytes calldata data)
+        external
+        pure
+        returns (uint24 fee)
+    {
+        // insert hook logic here
+        fee = 3000;
+    }
+
+    function beforeModifyPosition(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.ModifyPositionParams calldata params,
+        bytes calldata
+    ) external override returns (bytes4 selector) {
+        // insert hook logic here
+
+        selector = BaseHook.beforeModifyPosition.selector;
+    }
+
+    function afterModifyPosition(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.ModifyPositionParams calldata params,
+        BalanceDelta delta,
+        bytes calldata
+    ) external override returns (bytes4 selector) {
+        // insert hook logic here
+
+        selector = BaseHook.afterModifyPosition.selector;
+    }
+
+    function beforeSwap(address sender, PoolKey calldata key, IPoolManager.SwapParams calldata params, bytes calldata)
+        external
+        override
+        returns (bytes4 selector)
+    {
+        // insert hook logic here
+
+        selector = BaseHook.beforeSwap.selector;
+    }
+
+    function afterSwap(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.SwapParams calldata params,
+        BalanceDelta delta,
+        bytes calldata
+    ) external override returns (bytes4 selector) {
+        // insert hook logic here
+
+        selector = BaseHook.afterSwap.selector;
+    }
+}
+
+contract DynamicFeeFactory is BaseFactory {
+    constructor()
+        BaseFactory(
+            address(
+                uint160(
+                    Hooks.BEFORE_MODIFY_POSITION_FLAG | Hooks.AFTER_MODIFY_POSITION_FLAG | Hooks.BEFORE_SWAP_FLAG
+                        | Hooks.AFTER_SWAP_FLAG
+                )
+            )
+        )
+    {}
+
+    function deploy(IPoolManager poolManager, bytes32 salt) public override returns (address) {
+        return address(new DynamicFeeHook{salt: salt}(poolManager));
+    }
+
+    function _hashBytecode(IPoolManager poolManager) internal pure override returns (bytes32 bytecodeHash) {
+        bytecodeHash = keccak256(abi.encodePacked(type(DynamicFeeHook).creationCode, abi.encode(poolManager)));
+    }
+}
---
+++ src/hooks/MyHook.sol
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import {Hooks} from "@uniswap/v4-core/contracts/libraries/Hooks.sol";
+import {IHookFeeManager} from "@uniswap/v4-core/contracts/interfaces/IHookFeeManager.sol";
+import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
+import {PoolKey, PoolId, PoolIdLibrary} from "@uniswap/v4-core/contracts/types/PoolId.sol";
+import {BalanceDelta} from "@uniswap/v4-core/contracts/types/BalanceDelta.sol";
+import {BaseHook} from "v4-periphery/BaseHook.sol";
+
+import {BaseFactory} from "../BaseFactory.sol";
+
+contract MyHook is BaseHook, IHookFeeManager {
+    using PoolIdLibrary for PoolKey;
+
+    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
+
+    function getHooksCalls() public pure override returns (Hooks.Calls memory) {
+        return Hooks.Calls({
+            beforeInitialize: true,
+            afterInitialize: true,
+            beforeModifyPosition: true,
+            afterModifyPosition: true,
+            beforeSwap: true,
+            afterSwap: true,
+            beforeDonate: true,
+            afterDonate: true
+        });
+    }
+
+    function beforeInitialize(address sender, PoolKey calldata key, uint160 sqrtPriceX96, bytes calldata)
+        external
+        override
+        returns (bytes4 selector)
+    {
+        // insert hook logic here
+
+        selector = BaseHook.beforeInitialize.selector;
+    }
+
+    function afterInitialize(address sender, PoolKey calldata key, uint160 sqrtPriceX96, int24 tick, bytes calldata)
+        external
+        override
+        returns (bytes4 selector)
+    {
+        // insert hook logic here
+
+        selector = BaseHook.afterInitialize.selector;
+    }
+
+    function beforeModifyPosition(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.ModifyPositionParams calldata params,
+        bytes calldata
+    ) external override returns (bytes4 selector) {
+        // insert hook logic here
+
+        selector = BaseHook.beforeModifyPosition.selector;
+    }
+
+    function afterModifyPosition(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.ModifyPositionParams calldata params,
+        BalanceDelta delta,
+        bytes calldata
+    ) external override returns (bytes4 selector) {
+        // insert hook logic here
+
+        selector = BaseHook.afterModifyPosition.selector;
+    }
+
+    function beforeSwap(address sender, PoolKey calldata key, IPoolManager.SwapParams calldata params, bytes calldata)
+        external
+        override
+        returns (bytes4 selector)
+    {
+        // insert hook logic here
+
+        selector = BaseHook.beforeSwap.selector;
+    }
+
+    function afterSwap(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.SwapParams calldata params,
+        BalanceDelta delta,
+        bytes calldata
+    ) external override returns (bytes4 selector) {
+        // insert hook logic here
+
+        selector = BaseHook.afterSwap.selector;
+    }
+
+    function beforeDonate(address sender, PoolKey calldata key, uint256 amount0, uint256 amount1, bytes calldata)
+        external
+        override
+        returns (bytes4 selector)
+    {
+        // insert hook logic here
+
+        selector = BaseHook.beforeDonate.selector;
+    }
+
+    function afterDonate(address sender, PoolKey calldata key, uint256 amount0, uint256 amount1, bytes calldata)
+        external
+        override
+        returns (bytes4 selector)
+    {
+        // insert hook logic here
+
+        selector = BaseHook.afterDonate.selector;
+    }
+
+    function getHookFees(PoolKey calldata key) external view returns (uint24 fee) {
+        fee = 0;
+    }
+}
+
+contract MyHookFactory is BaseFactory {
+    constructor()
+        BaseFactory(
+            address(
+                uint160(
+                    Hooks.BEFORE_INITIALIZE_FLAG | Hooks.AFTER_INITIALIZE_FLAG | Hooks.BEFORE_MODIFY_POSITION_FLAG
+                        | Hooks.AFTER_MODIFY_POSITION_FLAG | Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG
+                        | Hooks.BEFORE_DONATE_FLAG | Hooks.AFTER_DONATE_FLAG
+                )
+            )
+        )
+    {}
+
+    function deploy(IPoolManager poolManager, bytes32 salt) public override returns (address) {
+        return address(new MyHook{salt: salt}(poolManager));
+    }
+
+    function _hashBytecode(IPoolManager poolManager) internal pure override returns (bytes32 bytecodeHash) {
+        bytecodeHash = keccak256(abi.encodePacked(type(MyHook).creationCode, abi.encode(poolManager)));
+    }
+}
