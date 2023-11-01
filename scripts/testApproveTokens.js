33eabf5b5de3b3e65936553794531f14ead36743
fksyuan <fksyuan@163.com>
2023-11-01 15:48:43 +0800

    update approve token address in approve token script

--- scripts/testApproveTokens.js
+++ scripts/testApproveTokens.js
@@ -9,12 +9,12 @@ async function main() {
     deployer.address
     );
 
-    const routerAddress = "0xc9b35F137F2C6f1C1DA14B73e71601238213085C";
-    const token1address="0x855633649b91F9A6146aeD9CcC18cd617c8e3C3A";
+    const routerAddress = "0xE6ae84584D424f0cf21Dd727150ce9C1aB5962a9";
+    const token1address="0x146F8F5622BA97D62dAd8C9aD6C2011cEc524922";
     const token1contract = await ethers.getContractAt("MockToken", token1address);
     const res1 = await token1contract.approve(routerAddress, '0x845951614014880000000');
     console.log("token1 approve res1=", res1);
-    const token2address="0xeccef7ccA92edF8a42B314273D96ad75fE485a8a";
+    const token2address="0xb61872f098D5E2B76b8333fbadaed41C02124DE7";
     const token2contract = await ethers.getContractAt("MockToken", token2address);
     const res2 = await token2contract.approve(routerAddress, '0x845951614014880000000');
     console.log("token2 approve res2=", res2);
